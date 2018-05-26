81a9cf4c424dd88efa68330a91239b085dfa7a3a
Imtiaj Ahammad <imtiajahammad.official@gmail.com>
2018-05-27 03:52:38 +0600

    teacherCreditToBeTaken type changed to int from double

--- UniversityManagementSystem05/Models/TeacherModel.cs
+++ UniversityManagementSystem05/Models/TeacherModel.cs
@@ -11,10 +11,10 @@ namespace UniversityManagementSystem05.Models
         public string teacherName { get; set; }
         public string teacherAddress { get; set; }
         public string teacherEmail { get; set; }
-        public int teacherNumber { get; set; }
+        public string teacherNumber { get; set; }
         public string teacherDesignation { get; set; }
         public string teacherDepartment { get; set; }
-        public double teacherCreditToBeTacken { get; set; }
+        public int teacherCreditToBeTaken { get; set; }
 
     }
 }
