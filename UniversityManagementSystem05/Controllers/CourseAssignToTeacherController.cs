13d5524bbd6d89abdacdfe26fade9f732511f935
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2019-02-24 23:09:10 +0600

    1.  ActionResult ViewAllAssignedTeachers()- CHANGED FROM Index.. 2. ActionResult AssignTeacher(CourseAssignToTeacherModel courseAssignToTeacherModel) - modified 3.  JsonResult CheckTakenCreditOverFlowsRemainingCredit(int teacherID,int currentCreditTaking) -added 4. some commenting added

--- UniversityManagementSystem05/Controllers/CourseAssignToTeacherController.cs
+++ UniversityManagementSystem05/Controllers/CourseAssignToTeacherController.cs
@@ -14,10 +14,8 @@ namespace UniversityManagementSystem05.Controllers
     public class CourseAssignToTeacherController : Controller
     {
         CourseAssignManager aCourseAssignManager = new CourseAssignManager();
-        
-
         // GET: CourseAssignToTeacher
-        public ActionResult Index()
+        public ActionResult ViewAllAssignedTeachers()
         {
             return View();
         }
@@ -33,15 +31,36 @@ namespace UniversityManagementSystem05.Controllers
         [HttpPost]
         public ActionResult AssignTeacher(CourseAssignToTeacherModel courseAssignToTeacherModel)
         {
-
-
             ViewBag.Departments = aCourseAssignManager.GetAllDepartments();
             ViewBag.Teachers = aCourseAssignManager.GetAllTeachers();
             ViewBag.CourseCodeList = aCourseAssignManager.GetAllCourseCodes();
+            //do the code to store in the database
+
+            int stored = aCourseAssignManager.SaveCourseAssignToTeacher(courseAssignToTeacherModel);
+            if (stored == 1)
+            {
+                ViewBag.Message=("Course Assign to Teacher done successfully");
+            }
+            else
+            {
+                ViewBag.Message=("Course Assign to Teacher Error");
+            }
             return View();
+
+            /* 
+             1. one course should be assigned to one teacher only
+             2. if teacher credit exceed, an dialog box with yes/no pop up and work accordingly
+             3. remaining credit returning false count
+             */
         }
 
-        [HttpPost]
+
+
+        /// <summary>
+        /// to get teacher list from Dept id
+        /// </summary>
+        /// <param name="deptId"></param>
+        /// <returns></returns>
         public JsonResult GetTeacherByDeptId(int deptId)
         {
             List<TeacherModel> teachers = new List<TeacherModel>();
@@ -58,13 +77,21 @@ namespace UniversityManagementSystem05.Controllers
             return Json(courseList, JsonRequestBehavior.AllowGet);
         }
 
-        [HttpPost]
+        /// <summary>
+        /// get teacher credit from teacher table
+        /// </summary>
+        /// <param name="teacherId"></param>
+        /// <returns></returns>
         public JsonResult GetCreditByTeacherId(int teacherId)
         {
             int creditToBeTaken = aCourseAssignManager.GetCreditByTeacherId(teacherId);
             return Json(creditToBeTaken, JsonRequestBehavior.AllowGet);
         }
-
+        /// <summary>
+        /// checks how much credits are remaining
+        /// </summary>
+        /// <param name="teacherId"></param>
+        /// <returns>total remaining credit </returns>
         public JsonResult GetRemainingCreditByTeacherId(int teacherId)
         {
             int creditToBeTaken= aCourseAssignManager.GetCreditByTeacherId(teacherId);
@@ -80,9 +107,30 @@ namespace UniversityManagementSystem05.Controllers
             }
             else
             {
-                remainingCredit = 0;
+                remainingCredit = creditToBeTaken - totalAssignedCredit;
             }
             return Json(remainingCredit, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// check if teacherAssignedCredit is more than the credit is being taken
+        /// </summary>
+        /// <param name="teacherID"></param>
+        /// <param name="currentCreditTaking"></param>
+        /// <returns></returns>
+        public JsonResult CheckTakenCreditOverFlowsRemainingCredit(int teacherID,int currentCreditTaking)
+        {
+            int creditToBeTaken = aCourseAssignManager.GetCreditByTeacherId(teacherID);
+            int totalAssignedCredit = aCourseAssignManager.GetAssignedCreditByTeacherId(teacherID);
+            totalAssignedCredit = totalAssignedCredit + currentCreditTaking;
+            if (totalAssignedCredit > creditToBeTaken)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
         }
 
 
@@ -93,11 +141,7 @@ namespace UniversityManagementSystem05.Controllers
             aCourseModel = aCourseAssignManager.GetCourseModelByCourseId(courseId);
             return Json(aCourseModel,JsonRequestBehavior.AllowGet);
         }
-
-
-
-
-
+    
         [HttpGet]
         public ActionResult EditAssignedTeacher(int id, int? message)
         {
